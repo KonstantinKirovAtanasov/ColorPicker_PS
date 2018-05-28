26c2ffe6811e3ab937737ee8a0a78ef31e9c3bd1
KonstantinKirovAtanasov <39441731+KonstantinKirovAtanasov@users.noreply.github.com>
2018-05-29 00:13:58 +0300

    Add files via upload

---
+++ CP_Konstantin/BindingInformation.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CP_Konstantin
+{
+    public class BindingInformation
+    {
+        public BindingInformation()
+        {}
+        private SolidColorBrush mouseOverColor = new SolidColorBrush();
+        public SolidColorBrush MouseOverColor
+        {
+            get { return mouseOverColor; }
+            set { mouseOverColor = value; }
+        }
+        private SolidColorBrush selectedColor = new SolidColorBrush();
+        public SolidColorBrush SelectedColor
+        {
+            get { return selectedColor; }
+            set { selectedColor = value; }
+        }
+        private string rGB;
+        public string RGB
+        {
+            get {
+                SetRGB();
+                return rGB; }
+            set { ; }
+        }
+        private string hEX;
+        public string HEX
+        {
+            get {
+                SetHEX();
+                return hEX; }
+            set { ; }
+        }
+        private string colorReport;
+        public string ColorReport
+        {
+            get { return colorReport; }
+            set {; }
+        }
+
+        private void SetRGB()
+        {
+            rGB = "Red, Green, Blue, Alpha = [" + mouseOverColor.Color.R + "," +
+                mouseOverColor.Color.G + "," + mouseOverColor.Color.B + "," + mouseOverColor.Color.A + "]";
+        }
+        private void SetHEX()
+        {
+            hEX = ("HEX Code = #" + mouseOverColor.Color.R.ToString("X") +
+               mouseOverColor.Color.G.ToString("X") + mouseOverColor.Color.B.ToString("X")).ToUpper();
+        }
+        public void SetBrightness(double Coeficient)
+        {
+            byte a = CheckLimitAndReturnValue(selectedColor.Color.A + Coeficient);
+            byte r = CheckLimitAndReturnValue(selectedColor.Color.R + Coeficient);
+            byte g = CheckLimitAndReturnValue(selectedColor.Color.G + Coeficient);
+            byte b = CheckLimitAndReturnValue(selectedColor.Color.B + Coeficient);
+            mouseOverColor.Color = Color.FromArgb(a, r, g, b);
+        }
+        public void SetLumia(double Coeficient)
+        {
+            byte a = CheckLimitAndReturnValue(selectedColor.Color.A);
+            byte r = CheckLimitAndReturnValue(selectedColor.Color.R+0.299*Coeficient);
+            byte g = CheckLimitAndReturnValue(selectedColor.Color.G+0.587*Coeficient);
+            byte b = CheckLimitAndReturnValue(selectedColor.Color.B+0.73*Coeficient);
+            mouseOverColor.Color = Color.FromArgb(a, r, g, b);
+        }
+        public void MakeColorReport()
+        {
+            double rPrim = ((double)mouseOverColor.Color.R ) / 255;
+            double gPrim = ((double)mouseOverColor.Color.G )/ 255;
+            double bPrim = ((double)mouseOverColor.Color.B )/ 255;
+            double K = Compare(rPrim, gPrim, bPrim);
+            StringBuilder report = new StringBuilder();
+            report.Append("Color Report:\n\n");
+            report.Append(rGB + "\n");
+            report.Append(hEX+"\n");
+            report.Append("Cyan , Magenta, Yellow, Black Key\n");
+            report.Append(("(C,M,Y,K)  = ["+ K.ToString("0.####")+" , "+
+                ((1-rPrim-K)/(1-K)).ToString("0.####") + " , " +
+                ((1 - gPrim - K) / (1 - K)).ToString("0.####") + " , " +
+                ((1 - bPrim - K) / (1 - K)).ToString("0.####") + "]\n\n")); //CMYK
+            report.Append("Date :"+ DateTime.Now);
+            colorReport = report.ToString();
+        }
+        private byte CheckLimitAndReturnValue(double ComponentValue)
+        {
+            byte temp = 0;
+            if (ComponentValue <= 255 && ComponentValue >= 0)
+                temp = (byte)(ComponentValue);
+            else if (ComponentValue >= 255)
+                temp = 255;
+            else if (ComponentValue <= 0)
+                temp = 0;
+            return temp;
+        }
+        private double Compare(double r, double g, double b)
+        {
+            if (r - Math.Max(g,b) > 0.0001)
+                 return r;
+            if (g - Math.Max(r,b) > 0.0001)
+                return g;
+            if (b - Math.Max(g, r) > 0.0001)
+                return b;
+            else
+                return 0;
+        }
+    }
+}
---
+++ CP_Konstantin/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace CP_Konstantin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        byte[] pixelInformation = new byte[4];
+        private string imagePath = "images\\Colors.png";
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set { imagePath = value; }
+        }
+
+        private BindingInformation bindingInfo = new BindingInformation();
+        public BindingInformation BindingInfo
+        {
+            get { return bindingInfo; }
+            set { bindingInfo = value; }
+        }
+
+        private void Colors_MouseOver(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                BitmapImage image = new BitmapImage(new Uri(Colors.Source.ToString()));
+                CroppedBitmap pixel = new CroppedBitmap(image, new Int32Rect((int)e.GetPosition(Colors).X,
+                    (int)e.GetPosition(Colors).Y, 1, 1));
+                pixel.CopyPixels(pixelInformation, 4, 0);
+                bindingInfo.MouseOverColor = new SolidColorBrush(Color.FromArgb(pixelInformation[3],
+                    pixelInformation[2], pixelInformation[1], pixelInformation[0]));
+                DataContext = null;
+                DataContext = this;
+            }
+            catch (ArgumentException)
+            {
+                // Do nothing ;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+        }
+
+        private void Colors_Click(object sender, MouseButtonEventArgs e)
+        {
+            bindingInfo.SelectedColor = new SolidColorBrush(Color.FromArgb(pixelInformation[3],
+                pixelInformation[2], pixelInformation[1], pixelInformation[0]));
+            bindingInfo.MakeColorReport();
+            Brightness.Value = 0;
+            Lumia.Value = 0;
+            DataContext = null;
+            DataContext = this;
+        }
+
+        private void Colors_MouseLeave(object sender, MouseEventArgs e)
+        {
+            bindingInfo.MouseOverColor.Color = bindingInfo.SelectedColor.Color;
+            DataContext = null;
+            DataContext = this;
+        }
+
+        private void GlobalMouseMove(object sender, MouseEventArgs e)
+        {
+            bindingInfo.MouseOverColor.Color = Color.FromArgb((byte)AlphaSlider.Value,
+               (byte)RedSlider.Value, (byte)GreenSlider.Value, (byte)BlueSlider.Value);
+            DataContext = null;
+            DataContext = this;
+        }
+
+        private void Grid3MouseLeave(object sender, MouseEventArgs e)
+        {
+            bindingInfo.SelectedColor.Color = bindingInfo.MouseOverColor.Color;
+            bindingInfo.MakeColorReport();
+        }
+
+        private void SetBrightness(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            bindingInfo.SetBrightness(Brightness.Value);
+            DataContext = null;
+            DataContext = this;
+        }
+
+        private void SetLumia(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            bindingInfo.SetLumia(Lumia.Value);
+            DataContext = null;
+            DataContext = this;
+        }
+
+        private void MouseLeaveBrightness(object sender, MouseEventArgs e)
+        {
+            bindingInfo.SelectedColor.Color = bindingInfo.MouseOverColor.Color;
+            bindingInfo.MakeColorReport();
+        }
+
+        private void MouseLeaveLumia(object sender, MouseEventArgs e)
+        {
+            bindingInfo.SelectedColor.Color = bindingInfo.MouseOverColor.Color;
+            bindingInfo.MakeColorReport();
+        }
+
+        private void RandomColor_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            bindingInfo.MouseOverColor.Color = Color.FromRgb(
+                (byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255));
+            bindingInfo.SelectedColor.Color = bindingInfo.MouseOverColor.Color;
+            bindingInfo.MakeColorReport();
+            Brightness.Value = 0;
+            Lumia.Value = 0;
+            DataContext = null;
+            DataContext = this;
+        }
+
+        private void GetColorReport_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(bindingInfo.ColorReport);
+        }
+    }
+}
